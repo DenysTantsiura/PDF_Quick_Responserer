24375ae2197ef8d7bec627d36406b5f83c7d778c
Denys Tantsiura <ghost0002501@gmail.com>
2023-09-15 23:33:10 +0300

    test-file-upload-ok, get-text-ok.

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ import pathlib
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.staticfiles import StaticFiles
+from PyPDF2 import PdfReader
 import uvicorn
 
 from src.conf import messages
 from src.routes import notes, tags
+
+
+
+def get_txt_from_pdf(file: str) -> str:
+    # creating a pdf reader object
+    reader = PdfReader(file)
+    text = ''
+
+    for page in range(len(reader.pages)):
+        text += reader.pages[page].extract_text()
+
+    return text
+
 
 app = FastAPI()
 
@@ -15,12 +29,16 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File()):
+async def create_upload_file(file: UploadFile = File()) -> dict:
     pathlib.Path("uploads").mkdir(exist_ok=True)
     file_path = f"uploads/{file.filename}"
     with open(file_path, "wb") as f:
         f.write(await file.read())
-    return {"file_path": file_path}
+
+    text = get_txt_from_pdf(file_path) # 'example.pdf'
+    print(text)
+
+    return {"file_text": text}  # file_path
 
 
 @app.get('/')
